6130585786569b86efb19cf92a8f9a3f7cd8504b
Kenneth Mahlatse Mokhethi <kennethmahlatsemokhethi@gmail.com>
2019-10-10 10:11:06 +0200

    Added another spec for person class

--- spec/personSpec.js
+++ spec/personSpec.js
@@ -1,5 +1,5 @@
 describe("The person class", () => {
-  it("Should return the person details suc as name,age,gender and interest", () => {
+  it("Should return the person details such as name,age,gender and interest", () => {
     //    Instantiating the object
     let person = new Person("Mike", 20, "male", [
       "reading",
@@ -10,6 +10,21 @@ describe("The person class", () => {
     let greeting = person.hello();
     expect(greeting).toBe(
       "Hello my name is Mike and I am 20 years old.My interest are reading,soccer and hockey."
+    );
+  });
+});
+
+describe("Another person", () => {
+  it("Should return the person details such as name,age,gender and interest", () => {
+    let person2 = new Person("Wendy", 24, "females", [
+      "netball",
+      "jogging",
+      "drawing"
+    ]);
+
+    let greeting2 = person2.hello();
+    expect(greeting2).toBe(
+      "Hello my name is Wendy and I am 24 years old.My interest are netball,jogging and drawing."
     );
   });
 });
